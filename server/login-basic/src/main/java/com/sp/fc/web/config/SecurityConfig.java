6f5406ea35925360a777ca3a89af176ff2d4c909
KimDongHyuk <kdhhdk83@gmail.com>
2022-05-27 01:42:27 +0900

    스프링 시큐리티 적용 로그인 방법예제

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,86 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    private final CustomAuthDetails customAuthDetails;
+    /*로그인이 먹지 않는 경우에는 CSRF 필터가 걸려 있기 때문이다. csrf 토큰발생 */
+    CsrfFilter csrfFilter;
+    UsernamePasswordAuthenticationFilter filter;
+
+    public SecurityConfig(CustomAuthDetails customAuthDetails) {
+        this.customAuthDetails = customAuthDetails;
+    }
+
+    /*어드민은 유저가 접근할 수 있는 모든 페이지 접근가능. */
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder() /*password 인코드 로컬에 한정해서 사용*/
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                ).withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("2222")
+                                .roles("ADMIN")
+                );
+
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(request->{
                     request
-                            .antMatchers("/**").permitAll()
+                            .antMatchers("/").permitAll()
+                            .anyRequest().authenticated()
                             ;
                 })
+                .formLogin(
+                        login->login.loginPage("/login")
+                                .permitAll() /*퍼밋올이 없을 경우 무한 루프에 빠질 수 있음.*/
+                                .defaultSuccessUrl("/", false)
+                                .failureUrl("/login-error")
+                                .authenticationDetailsSource(customAuthDetails)
+                )
+                .logout(logout->logout.logoutSuccessUrl("/"))
+                .exceptionHandling(exception->exception.accessDeniedPage("/access-denied"))
                 ;
+    }
+
+    /*web resources 필터를 타지 않도록 ignoring */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
     }
 }
